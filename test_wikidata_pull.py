f7b3fc90c1b401b798fa5e926c4eb759e7981e57
Leo552 <leo@terminalconvergence.com>
2020-06-17 17:15:23 +0100

    Added more cases to the testing of qwikidata test

--- test_wikidata_pull.py
+++ test_wikidata_pull.py
@@ -6,10 +6,10 @@ import pytest
 
 # Test the query function
 @pytest.mark.parametrize('q, result',[
-    ('Q8016', 'English people'),
-    ('Q257911', 'African Americans')])
+    ('Q8016', 'English people'), # Q8016 Winston Churchill - English people
+    ('Q257911', 'African Americans'),
+    ('Q704954', 'Jewish people')])
 def test_q_wikidata(q, result):
-    # Q8016 Winston Churchill - ethnic group - English people
     assert q_wikidata(q) == result
 
 if __name__ == '__main__':
