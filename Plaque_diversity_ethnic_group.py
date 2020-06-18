1afa4c45b8d82d5ac296a716e91db7a96fc47961
Leo552 <56204370+Leo552@users.noreply.github.com>
2020-06-18 21:21:36 +0100

    6 setup GitHub actions to auto run tests (#12)
    * Updated ethnic group python file into functions
    
    * Initial unit test of qwikidata
    
    * Spelt something wrong
    
    All test pass in the current setup.
    Using an array to test multiple cases
    
    * Added more cases to the testing of qwikidata test
    
    * Update environment.yml to include pytest
    
    * Create setup.py file for dependecy installation
    
    * Create requirements.txt file using pip
    
    * Can install a dependency when testing
    
    mkl-fft
    mkl-random
    mkl-service
    
    There dependencies have been removed - I guess I am hoping they aren't needed
    
    * Removing dependencies that can't be installed
    
    pywin32==227
    pywin32-ctypes==0.2.0
    pywinpty==0.5.7
    
    * Conda created the requirements file this time
    
    * Trying with pip with libraries removed
    
    * Rtree removed
    
    * removed sip
    
    Co-authored-by: Leo552 <leo@terminalconvergence.com>

--- Plaque_diversity_ethnic_group.py
+++ Plaque_diversity_ethnic_group.py
@@ -5,58 +5,74 @@ import time
 from qwikidata.sparql import (get_subclasses_of_item,
                               return_sparql_query_results)
 
-# Set the filename of the csv for analysis - same directory
-FILENAME = 'open-plaques-subjects-london.csv'
-df = pd.read_csv(FILENAME)
-
-# Remove the columns which don't have wikidata id's attached
-df.dropna(subset = ['wikidata_id'], inplace = True)
-
-# Store the results in a list
-results = []
-print('Total wikidata examples {0}'.format(df.shape[0]))
-
-# Iterate throught the wiki IDs
-for i, row in df.iterrows():
+def get_ethnicity(filename):
+    # Set the filename of the csv for analysis - same directory
+    df = pd.read_csv(filename)
     
-    # Waiting will mean more of the queries come back with results
-    time.sleep(1)
+    # Remove the columns which don't have wikidata id's attached
+    df.dropna(subset = ['wikidata_id'], inplace = True)
     
+    # Store the results in a list
+    results = []
+    print('Total wikidata examples {0}'.format(df.shape[0]))
+    
+    # Iterate throught the wiki IDs
+    for i, row in df.iterrows():
+        
+        # Waiting will mean more of the queries come back with results
+        time.sleep(1)
+        
+        res = q_wikidata(row['wikidata_id'])
+        
+        if not res == None:
+            results.append(res)
+            print(i, end = ',')
+            print(row['wikidata_id'])
+    
+    return results
+
+def q_wikidata(wiki_id):
     # P172 represents the ethnicity
     sparql_query = """
     SELECT ?item ?itemLabel 
     WHERE 
     {
-      wd:""" + row['wikidata_id'] + """ wdt:P172 ?item.
+      wd:""" + wiki_id + """ wdt:P172 ?item.
       SERVICE wikibase:label {bd:serviceParam wikibase:language "en" . }
     }
     """
     
-    # Use a try to avoid errors where the property field for cizenship isn't present e.g. objects
+    # Use a try to avoid errors where the property field for cizenship isn't 
+    # present e.g. objects
     try:
-        res = return_sparql_query_results(sparql_query)
-        results.append(res['results']['bindings'][0]['itemLabel']['value'])
-        print(i, end = ',')
+        res_all = return_sparql_query_results(sparql_query)
+        return res_all['results']['bindings'][0]['itemLabel']['value']
     except:
-        continue
-
-# Condense the list into a frequency dictionary
-res_dict = {}
-for citizen in results:
-    try:
-        res_dict[citizen] += 1
-    except:
-        res_dict[citizen] = 1
-
-y_pos = list(range(len(res_dict)))
-plt.rcParams["figure.figsize"] = (16,9)
-fig = plt.figure()
-plt.barh(y_pos, res_dict.values())
-plt.yticks(y_pos, res_dict.keys())
-
-fig.suptitle('Ethnic group of plaques in London',  fontsize = 20)
-plt.xlabel('Frequency', fontsize = 15)
-plt.ylabel('Ethnic group',  fontsize = 15)
-
-plt.show()
-fig.savefig('Ethnic group horizontal bar chart')
+        return None
+        
+        
+def plot(results):
+    # Condense the list into a frequency dictionary
+    res_dict = {}
+    for citizen in results:
+        try:
+            res_dict[citizen] += 1
+        except:
+            res_dict[citizen] = 1
+    
+    y_pos = list(range(len(res_dict)))
+    plt.rcParams["figure.figsize"] = (16,9)
+    fig = plt.figure()
+    plt.barh(y_pos, res_dict.values())
+    plt.yticks(y_pos, res_dict.keys())
+    
+    fig.suptitle('Ethnic group of plaques in London',  fontsize = 20)
+    plt.xlabel('Frequency', fontsize = 15)
+    plt.ylabel('Ethnic group',  fontsize = 15)
+    
+    plt.show()
+    fig.savefig('Ethnic group horizontal bar chart')
+    
+if __name__ == '__main__':
+    results = get_ethnicity('open-plaques-subjects-london.csv')
+    plot(results)
