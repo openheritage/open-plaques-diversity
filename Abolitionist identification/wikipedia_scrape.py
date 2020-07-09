d2b7335387ae5f0427777c5cc25fd498416b34d3
Leo552 <leo@terminalconvergence.com>
2020-07-09 13:00:16 +0100

    Re-organise the into file

---
+++ Abolitionist identification/wikipedia_scrape.py
@@ -0,0 +1,25 @@
+import wikipedia
+import pandas as pd
+
+FILENAME = 'open-plaques-subjects-london.csv'
+df = pd.read_csv(FILENAME)
+
+df.dropna(subset = ['en_wikipedia_url'], inplace = True)
+
+# Trigger words and there weightings
+
+def main():
+    for index, row in df.iterrows():
+        r = row['en_wikipedia_url']
+        
+def get_wiki_contents(url):
+    name = url.split('/')[-1]
+    try:
+        contents = wikipedia.page(name).content
+        return contents 
+    except:
+        print('Wiki retrieval failed {0}'.format(name))
+        return ''
+    
+if __name__ == '__main__':
+    main()
